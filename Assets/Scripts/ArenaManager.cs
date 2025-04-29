ecbb71aa87d10f177aa54c50b815ee4dc7111cb6
Simon SkvaraPC <simon.skvara@ue-germany.de>
2025-04-29 15:40:22 +0200

    Game logic done
    implemented the game logic, rounds, it is kind of brittle though

--- Assets/Scripts/ArenaManager.cs
+++ Assets/Scripts/ArenaManager.cs
@@ -5,6 +5,8 @@ public class ArenaManager : MonoBehaviour
 {
     public static ArenaManager Instance;
 
+    private bool _hasScored;
+
     private void Awake()
     {
         if (Instance == null)
@@ -12,7 +14,29 @@ public class ArenaManager : MonoBehaviour
             Instance = this;
         }
     }
-    
-    
-    
+
+    public void ResetArena()
+    {
+        _hasScored = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !_hasScored)
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            
+            if (player.player1)
+            {
+                SumoManager.Instance.Player2WinsRound();
+            }
+
+            if (player.player2)
+            {
+                SumoManager.Instance.Player1WinsRound();
+            }
+
+            _hasScored = true;
+        }
+    }
 }
--- Assets/Scripts/SumoManager.cs
+++ Assets/Scripts/SumoManager.cs
@@ -1,6 +1,14 @@
 using System;
+using System.Collections;
+using skv_toolkit;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+
+/// <summary>
+/// Sorry about all those texts, had only one day to do this
+/// </summary>
 public class SumoManager : MonoBehaviour
 {
     public static SumoManager Instance;
@@ -8,9 +16,24 @@ public class SumoManager : MonoBehaviour
     public GameObject player1;
     public GameObject player2;
 
-    private Transform _player1StartTransform;
-    private Transform _player2StartTransform;
+    private Vector3 _player1StartPosition;
+    private Vector3 _player2StartPosition;
     
+    private Quaternion _player1StartRotation;
+    private Quaternion _player2StartRotation;
+
+    public int player1RoundsWon;
+    public int player2RoundsWon;
+    private int _currentRound;
+    
+    [Header("Texts")] 
+    public TextMeshProUGUI player1RoundsWonText;
+    public TextMeshProUGUI player2RoundsWonText;
+    public GameObject player1WonTheRound;
+    public GameObject player2WonTheRound;
+    public GameObject player1WonTheGame;
+    public GameObject player2WonTheGame;
+    public GameObject endGamePanel;
 
     private void Awake()
     {
@@ -22,16 +45,92 @@ public class SumoManager : MonoBehaviour
 
     private void Start()
     {
-        _player1StartTransform = player1.transform;
-        _player2StartTransform = player2.transform;
+        UpdateText();
+        _player1StartPosition = player1.transform.position;
+        _player2StartPosition = player2.transform.position;
+
+        _player1StartRotation = player1.transform.rotation;
+        _player2StartRotation = player2.transform.rotation;
     }
 
     public void ResetPlayers()
     {
-        player1.transform.position = _player1StartTransform.position;
-        player1.transform.rotation = _player1StartTransform.rotation;
+        /*player1RoundsWon = 0;
+        player2RoundsWon = 0;*/
         
-        player2.transform.position = _player2StartTransform.position;
-        player2.transform.rotation = _player2StartTransform.rotation;
+        player1.transform.position = _player1StartPosition;
+        player1.transform.rotation = _player1StartRotation;
+        
+        player2.transform.position = _player2StartPosition;
+        player2.transform.rotation = _player2StartRotation;
+    }
+
+    public void Player1WinsRound()
+    {
+        player1RoundsWon++;
+        UpdateText();
+        
+        if (player1RoundsWon >= 3)
+        {
+            GameWin(true, false);
+            return;
+        }
+        
+        StartCoroutine(RoundWon());
+        player1WonTheRound.SetActive(true);
+        
+        Debug.Log("Player 1 wins the round");
+    }
+
+    public void Player2WinsRound()
+    {
+        player2RoundsWon++;
+        UpdateText();
+        
+        if (player2RoundsWon >= 3)
+        {
+            GameWin(false, true);
+            return;
+        }
+
+        StartCoroutine(RoundWon());
+        player2WonTheRound.SetActive(true);
+        
+        Debug.Log("Player 2 wins the round");
+    }
+
+    IEnumerator RoundWon()
+    {
+        yield return new WaitForSeconds(2f);
+        ArenaManager.Instance.ResetArena();
+        ResetPlayers();
+        
+        player1WonTheRound.SetActive(false);
+        player2WonTheRound.SetActive(false);
+    }
+
+    void UpdateText()
+    {
+        player1RoundsWonText.text = $"{player1RoundsWon}/3";
+        player2RoundsWonText.text = $"{player2RoundsWon}/3";
+    }
+
+    void GameWin(bool isPlayer1, bool isPlayer2)
+    {
+        UpdateText();
+
+        if (isPlayer1)
+        {
+            player1WonTheGame.SetActive(true);
+        }
+
+        if (isPlayer2)
+        {
+            player2WonTheGame.SetActive(true);
+        }
+        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        
     }
 }
